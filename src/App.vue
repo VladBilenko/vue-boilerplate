99a337bb4df17c7ba25e09eb81f1973aa964fe8d
vladbilenko <vladislav.bilenko-ae@transvoyant.com>
2020-02-23 14:00:32 +0200

    Add project skeleton

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Navigation></Navigation>
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Navigation from "@/components/Navigation";
+
+export default {
+  name: "Home",
+  components: {
+    Navigation
+  }
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
